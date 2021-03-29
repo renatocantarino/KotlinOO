18dbf3ba70c6a98279fe49b7aa43092c426aeda8
Renato Cantarino <cast.carlosc.utic@sebrae.com.br>
2021-03-29 10:59:02 -0300

    abstracts

--- src/main/kotlin/Models/Funcionario.kt
+++ src/main/kotlin/Models/Funcionario.kt
@@ -14,4 +14,8 @@ abstract class Funcionario(
 
    protected abstract fun  CalculaAuxilioAlimentacao(): Double
 
+    override fun toString(): String {
+        return "${nome}  ${salario}"
+    }
+
 }
